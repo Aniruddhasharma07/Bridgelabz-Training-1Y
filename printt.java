08f4703e593f4db7af43e05b343e808768cc5e94
Aniruddhasharma07 <sharmaaniruddha72@gmail.com>
2026-01-08 14:14:43 +0530

    Printing 1 to 100 program

---
+++ printt.java
@@ -0,0 +1,8 @@
+public class printt{
+    public static void main(String[] args) {
+        // Print numbers from 1 to 100
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
