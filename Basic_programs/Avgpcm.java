34593cfd9084b2d76b512fdf982ae66a33d1aa15
Aniruddhasharma07 <sharmaaniruddha72@gmail.com>
2026-01-15 23:52:46 +0530

    Assignment 2 level 1 problem 2

---
+++ Basic_programs/Avgpcm.java
@@ -0,0 +1,9 @@
+class Avgpcm{
+    public static void main(String[] args){
+        int m= 94;
+        int p= 95;
+        int c= 96;
+        double avgPCM= (m + p + c) / 3.0;
+        System.out.println("Sam's average mark in PCM is " + avgPCM);
+    }
+}
